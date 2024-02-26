839efb3cb758240e70a067280cba1da46eff803a
ohthisis <saitnyeinnaings@gmail.com>
2024-02-26 11:10:45 +0630

    first commit

---
+++ Day1/Lesson1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int data1=0;  //int   variable  = 0; %d
+float data2=3.14;  //%f
+//4bytes;
+double data3= 3.45;//8bytes
+char name='htun htun';   //1byte   %c / %s
+
+
+int main(){
+
+
+    return 0;
+}
+
+//to find data types in C   //
+
+//download VS code //youtube how to download vs code on windows 11
+//msquare programming
---
+++ Day2/Lesson1.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+
+int data1;
+float data2;
+//char email='A'; //1byte
+char data3[]="saitnaing";
+//char name[4]={'A','b','c','apple'};  //%c,%s //C မှာ string မရှိ array[]
+int numbers[5]={1,2,3,4,5};
+
+char email[20];
+char name[20];
+//char email[20];
+//const int number1=10;
+int main(){
+//    printf("the size of char is %d\n", sizeof(email));//
+//    printf("the size of char is %d\n", sizeof(name));
+//    printf("the email %c\n",email);
+//    printf("the name %s\n",name);
+//    printf("%c",name[3]);
+//    printf("Enter you name");
+//    scanf("%s",&name);
+//    printf("Enter your email");
+//    scanf("%s",&email);
+//
+//      printf("my name is %s and my email is %s",name,email);
+//    printf("Enter number 1");
+//    scanf("%d",&number1);
+//    printf("const number 1 is %d",number1);
+
+//github
+//vscode download
+
+    return 0;
+}
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ohthisis/C_programming_basic.git
+        git push -u origin main
